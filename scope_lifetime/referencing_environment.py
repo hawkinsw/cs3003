1d03db1c3d416cd35cfcda600ffdb69ed5a424ce
Will Hawkins <whh8b@obs.cr>
2021-09-03 02:14:00 -0400

    Update referencing environment demo

--- scope_lifetime/referencing_environment.py
+++ scope_lifetime/referencing_environment.py
@@ -7,6 +7,10 @@ def show_referencing_environment():
   print("\n".join(frame.f_locals.keys()))
 
 
+def testing():
+  five = 5
+  pass
+
 def outer_function(parameter_one = None, parameter_two = None):
   outer_local1 = None
   def inner_function(inner_parameter_one = None, inner_parameter_two = None):
@@ -16,8 +20,21 @@ def outer_function(parameter_one = None, parameter_two = None):
       pass
     inner_local1 = None
     inner_local2 = None
-    show_referencing_environment()
-    
+    def inner_inner_function():
+      nonlocal outer_local1
+      nonlocal outer_local2
+
+      nonlocal five
+
+      if inner_local1 == None:
+        pass
+      if inner_local2 == None:
+        pass
+      inner_inner_local1 = None
+      inner_inner_local2 = None
+      show_referencing_environment()
+    inner_inner_function()
+
   outer_local2 = None
   inner_function()
 
