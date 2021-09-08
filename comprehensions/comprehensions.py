972054a9edd843a20f35194d4957a1ad7733e1e4
Will Hawkins <whh8b@obs.cr>
2021-09-08 02:28:28 -0400

    Include head/tail examples.

--- comprehensions/comprehensions.py
+++ comprehensions/comprehensions.py
@@ -14,9 +14,26 @@ def squares_of_integers(low, high):
 def verbose_squares_of_integers(low, high):
   return [ f"{x}**2={x**2}" for x in range(low, high)]
 
+def split(lst):
+  return lst[0], lst[1:]
+
+def modern_split(lst):
+  head, *tail = lst
+  return head, tail
+
 if __name__=='__main__':
   print(f"all_integers(5, 10): {all_integers(5, 10)}")
   print(f"odd_integers(5, 10): {odd_integers(5, 10)}")
   print(f"even_integers(5, 10): {even_integers(5, 10)}")
   print(f"squares_of_integers(5, 10): {squares_of_integers(5, 10)}")
   print(f"verbose_squares_of_integers(5, 10): {verbose_squares_of_integers(5, 10)}")
+
+  lst = verbose_squares_of_integers(5,10)
+
+  split_head, split_tail = split(lst)
+  modern_head, modern_tail = modern_split(lst)
+
+  print(f"split_head: {split_head}")
+  print(f"split_tail: {split_tail}")
+  print(f"modern_head: {modern_head}")
+  print(f"modern_tail: {modern_tail}")
