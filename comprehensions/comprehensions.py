61e0f7612bd72c55c45f1b8adb6a230642276628
Will Hawkins <whh8b@obs.cr>
2021-09-11 00:05:29 -0400

    Add examples of comprehensions in Python.

--- comprehensions/comprehensions.py
+++ comprehensions/comprehensions.py
@@ -1,25 +1,24 @@
+""" Examples of writing list comprehensions in Python. """
 
 def all_integers(low, high):
+  """ Generate a list of all the integers between low and high. """
   return [x for x in range(low, high)]
 
 def odd_integers(low, high):
+  """ Generate a list of all odd integers between low and high. """
   return [x for x in range(low, high) if x % 2]
 
 def even_integers(low, high):
+  """ Generate a list of all even integers between low and high. """
   return [x for x in range(low, high) if not (x % 2)]
 
 def squares_of_integers(low, high):
+  """ Generate a list of the squares of all integers between low and high. """
   return [ x ** 2 for x in range(low, high)]
 
 def verbose_squares_of_integers(low, high):
+  """ Generate a list of equations for the squares of all integers between low and high. """
   return [ f"{x}**2={x**2}" for x in range(low, high)]
-
-def split(lst):
-  return lst[0], lst[1:]
-
-def modern_split(lst):
-  head, *tail = lst
-  return head, tail
 
 if __name__=='__main__':
   print(f"all_integers(5, 10): {all_integers(5, 10)}")
@@ -27,13 +26,3 @@ if __name__=='__main__':
   print(f"even_integers(5, 10): {even_integers(5, 10)}")
   print(f"squares_of_integers(5, 10): {squares_of_integers(5, 10)}")
   print(f"verbose_squares_of_integers(5, 10): {verbose_squares_of_integers(5, 10)}")
-
-  lst = verbose_squares_of_integers(5,10)
-
-  split_head, split_tail = split(lst)
-  modern_head, modern_tail = modern_split(lst)
-
-  print(f"split_head: {split_head}")
-  print(f"split_tail: {split_tail}")
-  print(f"modern_head: {modern_head}")
-  print(f"modern_tail: {modern_tail}")
