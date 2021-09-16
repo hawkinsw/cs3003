00bbddb05d78e0f9895311bc2644cf60427a6b5e
Will Hawkins <whh8b@obs.cr>
2021-09-16 03:22:18 -0400

    Destructuring examples in Python.

---
+++ pattern_matching/destruct.py
@@ -0,0 +1,15 @@
+a = 5
+b = 6
+lst = [1,2,3,4,5]
+first, *_, last = lst
+t = (a, b)
+aa, bb = t
+
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"aa: {aa}")
+print(f"bb: {bb}")
+
+print(f"lst: {lst}")
+print(f"first: {first}")
+print(f"last: {last}")
