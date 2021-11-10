50d68d803a3fb380c7224bfd1a5d644a9787ffaa
Will Hawkins <hawkinsw@obs.cr>
2021-11-09 23:35:50 -0500

    Update integer generation and natural number arithmetic.

--- prolog_intro_three/integer.pl
+++ prolog_intro_three/integer.pl
@@ -1,2 +1,7 @@
 generate_integer(0).
 generate_integer(X) :- generate_integer(Y), X is Y + 1.
+
+generate_integer_tr(X) :- next_integer(0,X).
+
+next_integer(J, J).
+next_integer(J, L) :- K is J + 1, next_integer(K, L).
--- prolog_intro_three/succ.pl
+++ prolog_intro_three/succ.pl
@@ -1,7 +1,13 @@
+natural_number(0).
+natural_number(succ(X)) :- natural_number(X).
+
+lt(0, succ(_)).
+lt(succ(X), succ(Y)) :- lt(X, Y).
+
 add(X, 0, X).
 add(X, succ(Y), succ(Z)) :- add(X, Y, Z).
 
-mult(succ(0), Y, Y).
+mult(0, _, 0).
 mult(succ(X), Y, Z) :- mult(X, Y, Zp), add(Y, Zp, Z).
 
 subt(A, B, C) :- add(B, C, A).
