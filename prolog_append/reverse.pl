0de99cd048276685f482d71a770abc0d38bfa2a9
Will Hawkins <whh8b@obs.cr>
2021-11-10 01:15:14 -0500

    Rename files and add an implementation of reverse.

--- prolog_append/reverse.pl
+++ prolog_append/reverse.pl
@@ -1,2 +1,10 @@
 reverse([], []).
 reverse([X|Xs], XsX) :- reverse(Xs, Sx), append(Sx, [X], XsX).
+
+reverse_acc(ToReverse, Reversed) :- reverse_acc_do(ToReverse, [], Reversed).
+
+reverse_acc_do([], Reversed, Reversed).
+reverse_acc_do([X|Xs], SoFar, Reversed) :- reverse_acc_do(Xs, [X|SoFar], Reversed).
+
+time_reverse_acc(X, Reversed) :- length(List, X), reverse_acc(List, Reversed).
+time_reverse(X, Reversed) :- length(List, X), reverse(List, Reversed).
