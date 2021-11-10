95048e03e1b808ea9bc3f0396b6652f8249ba891
Will Hawkins <whh8b@obs.cr>
2021-11-10 01:15:25 -0500

    Add implementation of permute.

---
+++ prolog_append/permute.pl
@@ -0,0 +1,7 @@
+my_append([], List, List).
+my_append([H|T], List, [H|AppendedList]) :- my_append(T, List, AppendedList).
+
+permute([], []).
+permute(List, [X|Xs]) :- append(W, [X|U], List),
+append(W, U, MinusX),
+permute(MinusX, Xs).
