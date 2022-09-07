809432fabbfb0c9697987668256b98680bb46c70
Will Hawkins <whh8b@obs.cr>
2022-09-07 13:36:08 -0400

    Add dynamic typing (Python, JavaScript) examples

---
+++ dynamic_type_js/index.js
@@ -0,0 +1,15 @@
+const {
+  BinarySearchTree,
+} = require('@datastructures-js/binary-search-tree')
+
+const print = console.log
+
+print("tree = \"Ash\"")
+tree = "Ash"
+print("typeof tree: " + typeof tree)
+print("tree.constructor.name: " + tree.constructor.name)
+print("\n\n\n")
+print("tree = new BinarySearchTree()")
+tree = new BinarySearchTree()
+print("typeof tree: " + typeof tree)
+print("tree.constructor.name: " + tree.constructor.name)
---
+++ dynamic_type_python/example.py
@@ -0,0 +1,8 @@
+from bintrees import BinaryTree
+
+if __name__ == "__main__":
+    tree = "Ash"
+    print(f"{type(tree)=}\n")
+
+    tree = BinaryTree()
+    print(f"{type(tree)=}\n")
