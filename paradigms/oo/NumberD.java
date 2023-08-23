966d6de226a5521f4c7e297ee6a4881898b4cef9
Will Hawkins <hawkinsw@obs.cr>
2023-08-22 22:19:27 -0400

    Rename NumberDocumented to NumberD.

--- paradigms/oo/NumberD.java
+++ paradigms/oo/NumberD.java
@@ -1,21 +1,21 @@
 /**
  * A class for calculating the integer root of a given number.
  */
-public class NumberDocumented {
+public class NumberD {
   /**
    * Construct an instance of the NumberDocumented class.
    * 
    * @param num The number for which to calculate the integer root.
    */
-  public NumberDocumented(int num) {
+  public NumberD(int num) {
     this.m_num = num;
   }
 
   /**
-   * Calculate the integer root of {@link NumberDocumented#m_num}.
+   * Calculate the integer root of {@link NumberD#m_num}.
    * Note the similarities between this implementation and the
    * imperative implementation. 
-   * @return The integer root of {@link NumberDocumented#m_num}.
+   * @return The integer root of {@link NumberD#m_num}.
    */
   public boolean integerRoot() {
     // Step from 0 (i) until i^2 is bigger than NumberDocumented#m_num ...
@@ -35,7 +35,7 @@ public class NumberDocumented {
    * @param args N/A.
    */
   public static void main(String args[]) {
-    NumberDocumented rootable = new NumberDocumented(36);
+    NumberD rootable = new NumberD(36);
 
     if (rootable.integerRoot()) {
       System.out.println("We have a root!\n");
