e89e1667a784a4ef01a9c08d951d20a5bb173b29
Will Hawkins <hawkinsw@obs.cr>
2023-09-05 22:32:52 -0400

    Update to better dynamic type example (Python)
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- dynamic_type_python/example.py
+++ dynamic_type_python/example.py
@@ -1,8 +1,11 @@
 from bintrees import BinaryTree
 
-if __name__ == "__main__":
-    tree = "Ash"
-    print(f"{type(tree)=}\n")
+class Address():
+    def __init__(self, street: str):
+        self.__street = street
 
-    tree = BinaryTree()
-    print(f"{type(tree)=}\n")
+if __name__ == "__main__":
+    pine = Address("349 Pine Dr.")
+    pine = BinaryTree()
+    print("")
+
