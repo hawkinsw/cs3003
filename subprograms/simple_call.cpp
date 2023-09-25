1a1bdf6f3d892a5b0cfdd308a9b3110bfdacd232
Will Hawkins <hawkinsw@obs.cr>
2023-09-25 12:35:45 -0400

    Add a simple call example.

---
+++ subprograms/simple_call.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void noleaf() {
+}
+
+void swap(int a, int b) {
+  int newa = a;
+  int newb = b;
+
+  ///
+  noleaf();
+  return;
+}
+
+int main() {
+  int a = 5;
+  int b = 7;
+  swap(a, b);
+  return 0;
+}
+
